f0a4d99bc4ad322965db9b7e1acd30a55f385015
Travis Griffiths <travis@travisgriffiths.com>
2023-06-30 17:53:52 -0400

    build with custome theme

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang='ts'>
+
+	// Your custom Skeleton theme:
+	import '../theme.postcss';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../app.postcss';
+
+
+	// // The ordering of these imports is critical to your app working properly
+	// import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	// import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// // Most of your app wide CSS should be put in this file
+	// import '../app.postcss';
+</script>
+
+<slot />
