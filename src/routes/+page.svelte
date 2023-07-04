2c50997ec57d027a33ed26ed9fff8f33f1d1ef8e
Travis Griffiths <travis@travisgriffiths.com>
2023-07-04 18:30:22 -0400

    set up headings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,4 +7,8 @@
 
 	The Dustlands are merely the wreckage of a once rich and verdant world, called Grabin, rendered to ruins and sand by the great Cataclysm. Civilization clings to the poles where the cold and icecaps allow for enough water to allow for grassy tundra. The equatorial regions are heat baked death zones full of nothing but rock, sand and scorching death...and the treasures of the World that Was.
 
+	<h2>Secondary</h2>
+
+
+	<h3>Tertiary</h3>
 </article>
