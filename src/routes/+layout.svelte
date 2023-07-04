4cc1fe795c4b22c03fcc48d1ed90231972955da0
Travis Griffiths <travis@travisgriffiths.com>
2023-07-04 18:04:45 -0400

    style headings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,14 +8,6 @@
 
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.postcss';
-
-
-	// // The ordering of these imports is critical to your app working properly
-	// import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-	// // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-	// import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// // Most of your app wide CSS should be put in this file
-	// import '../app.postcss';
 </script>
 
 <slot />
