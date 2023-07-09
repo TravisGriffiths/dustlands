1a0c2005ada26296b6c6e95de2e3e15594b37167
Travis Griffiths <travis@travisgriffiths.com>
2023-07-09 10:54:46 -0400

    fill out system

--- src/routes/system/+page.svelte
+++ src/routes/system/+page.svelte
@@ -6,6 +6,64 @@
 
    <hr>
 
-   The EZd6 system is played with 1 to 6 six sided dice. 
+   The <a href="https://www.ezd6.com/">EZd6 system</a> is played with 1 to 6 six sided dice, written by the amazing DM Scotty
+   available from <a href="https://www.drivethrurpg.com/browse.php?keywords=ezd6&x=0&y=0&author=&artist=&pfrom=&pto=">DriveThru RPG</a> 
+   is a minimalist system that has won endless awards and praise from both game reviewers and GMs alike. The system tries to 
+   keep all of the fun and stories of common RPGs while cutting the rules to the smallest set possible. The focus is to drive 
+   ineresting stories quickly, while making player choices central. 
+
+   <p></p>
+
+   <h2>Core Mechanic</h2>
+
+   Every task has a difficulty, and a target number for that task ranging from Easy (2) to Super Hard (6)
+   <ul>
+      <li>Easy (2)</li>
+      <li>Normal (3)</li>
+      <li>Hard (4)</li>
+      <li>Very Hard (5)</li>
+      <li>Super Hard (6)</li>
+   </ul>
+
+   Roll a six sided dice. If you roll the task target number, or higher, your character succeeds. 
+
+   <h2>Core Combat</h2>
+   Every opponent has a difficulty to hit just like tasks. For the average enemy, the target number is 3. Characters are normally
+   simply a 3 for any opponent to hit. Succeeding on an attack roll will always do a single <a href="strikes">strike</a> of 
+   damage on an opponent no matter what the weapon. <a href="crital-hits">Critical Hits</a> can do more damage, see below. 
+
+   <h2>Boons</h2>
+   When a character is particularly good at doing something, they get a "boon". A boon means you may roll 
+   <span class="font-bold">two</span> dice and take the <span class="font-bold">highest</span>. The target number to beat for 
+   the task is the same. 
+
+   <h2>Banes</h2>
+   When a character is attempting something unusually difficult, for example fighting in total darkness, they will have 
+   a "bane". A character with a bane rolls two dice and take the <span class="font-bold">lowest</span>. Banes cancel 
+   out boons, resulting in a normal one die roll. 
+
+   <h2>Failure</h2>
+   Failure is not all bad. While the character will not succeed at the task, any character failures earns a 
+   <a href="#karma">Karma</a> point. Lots of failure, leads to lots of karma which can save a character when 
+   they really need it. Karma is explained below. 
+
+   <h2>Critical Failures</h2>
+   Rolling a one is a critical failure. It means that not only did a character fail the task, but the GM may apply more secondary 
+   difficuties to the situation. Droping a weapon, running out of ammunition, alerting the guards are all examples. 
+   <span class="font-bold">Note:</span> the <span class="underline">KEPT</span> die must roll a one. Since a boon is rolling two and keeping the highest, to 
+   roll a critical failure while having a boon, means the player has to roll double ones. A skilled character might fail, but 
+   is unlikely to really screw up. Likewise a character working under difficult conditions, with a bane, has two chances to roll 
+   a critical failure, as either die can be a one, and will be kept.  
+
+   <h2 id="critial-success">Critical Success</h2>
+   Similar to critical failures, a 6 is always a success. In combat, hitting an opponent with a critical success does the normal 
+   strike of damage, and the player may roll to hit again. On the "confirming" roll, rolling a further six will do another strike, 
+   and allow a player to roll again to see if the critical hit continues. There is NO limit on how much damage this can cause. It is 
+   also totally possible to use <a href="karma">Karma</a> to "bump" these rolls, allowing massive critical strikes when a players really needs them, if 
+   they are willing to spend the resources on them. 
+
+   <h2 id="karma">Karma</h2>
+   Karma is a meta currency that all characters have. A character normally gets 3 karma points 
+
 
 </article>
