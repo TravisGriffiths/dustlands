0f1bc0a30cacf8516cfdfc8ffbe5425cd5936468
Travis Griffiths <travis@travisgriffiths.com>
2023-07-10 08:04:29 -0400

    add text decorator

---
+++ src/lib/components/T.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+   export let c: string = '';
+
+   let classStr: string = 'font-bold';
+   const getClassStr = (dec: string) => {
+      let classes: Array<string> = []
+      if (dec.match("b")) classes = [...classes, 'font-bold']
+      if (dec.match("s")) classes = [...classes, 'font-semibold']
+      if (dec.match("u")) classes = [...classes, 'underline']
+      return classes.join(' ')
+   }
+   $: classStr = getClassStr(c)
+</script>
+
+<span class={classStr}>
+   <slot />
+</span>
+
