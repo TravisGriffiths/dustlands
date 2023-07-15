f1f884e2589cc0118d13f4031ecbb7f3892a64a7
Travis Griffiths <travis@travisgriffiths.com>
2023-07-15 13:10:23 -0400

    Add some background

---
+++ src/routes/world/+page.svelte
@@ -0,0 +1,18 @@
+<article>
+
+   <section>
+      <h1>Grabin</h1>
+      A once verdant and advanced world of magic and technology rendered to ruins and sand by the great Cataclysm. Civilization clings to the poles where tempratures allow for enough water to allow for some grassland and rain. The equatorial regions are heat baked death zones full of nothing but rock, sand and scorching death...and the treasures of the World that Was.
+
+      <h2>The World That Was</h2>
+      The Cataclysm that broke the world happened almost a thousand years ago. Scholars argue about the exact dates and some argue that it was many separate cataclysms rather than a single one. In any event, from the rare wonders and ruins left behind we know that the world was a globe spanning highly magical society with wonders undreamed of in the blasted Age of Dust. There is some evidence that this magical power even allowed the citizens of the ante Cataclysmic world to travel and trade with other worlds and civilizations beyond our current understanding. Sadly, in those times the world was very different and the poles were considered something of an arctic wasteland, with little in the way of population or infrastructure. The great cities of the World that Was are now covered with dust and sand and are mostly atop the highest plateaus. A few madmen claim that the endless seas of sand were once actual seas full of water and that had depths of several miles. This is hard to credit today.
+
+
+      <h2>The Poles and Cosmology</h2>
+      The poles today are sufficiently cool to allow moisture left in the air to rain on the few grasslands left, from which a few great cities of the world today nourish their populations and economies. The seven great cities of the north each have a water source that supports them, and are surrounded by unusually verdant grasslands or biomes that support herds of animals on which the urban populations depend. Some even have what would be recognized as traditional agriculture.
+      
+      
+
+   </section>
+
+</article>
